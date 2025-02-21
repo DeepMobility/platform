8c57ecf12832e7c45c5576dc66fba229aafde1cf
forster <groux.forster@gmail.com>
2025-02-21 11:56:58 +0100

    update body parts and exercice types

--- src/lib/exerciseTypes.ts
+++ src/lib/exerciseTypes.ts
@@ -2,6 +2,7 @@ const exerciseTypes = [
   { value: 'balance', label: 'Équilibre' },
   { value: 'strengthening', label: 'Renforcement' },
   { value: 'stretching', label: 'Étirements' },
+  { value: 'breathing', label: 'Respiration' },
 ]
 
 export default exerciseTypes
--- src/lib/painfulBodyParts.ts
+++ src/lib/painfulBodyParts.ts
@@ -1,10 +1,14 @@
 const painfulBodyParts = [
+  { value: 'cervical', label: 'Cervicales' },
+  { value: 'shoulders', label: 'Épaules' },
+  { value: 'upper-back', label: 'Haut du dos' },
+  { value: 'elbows', label: 'Coudes' },
+  { value: 'wrists', label: 'Poignets' },
   { value: 'lower-back', label: 'Bas du dos' },
   { value: 'legs', label: 'Jambes' },
-  { value: 'shoulders', label: 'Épaules' },
-  { value: 'wrists', label: 'Poignets' },
-  { value: 'upper-back', label: 'Haut du dos' },
-  { value: 'cervical', label: 'Cervicales' },
+  { value: 'knees', label: 'Genoux' },
+  { value: 'ankles', label: 'Chevilles' },
+  { value: 'feet', label: 'Pieds' },
 ]
 
 export default painfulBodyParts
