b87a3011630f0206325bc4da19fe9d2902dcabac
forster <groux.forster@gmail.com>
2025-03-11 09:46:11 +0100

    remove dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Viewport, Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -17,6 +17,10 @@ export const metadata: Metadata = {
   description: "Pour vos routines musculaires",
   robots: { index: false }
 };
+
+export const viewport: Viewport = {
+  themeColor: 'only light',
+}
 
 export default async function RootLayout({
   children,
