6268809b460d0f4f17230826d319235d40abfaf5
forster <groux.forster@gmail.com>
2025-04-13 11:16:18 +0200

    update badges

--- src/lib/badgesList.ts
+++ src/lib/badgesList.ts
@@ -1,19 +1,19 @@
 const badgesList = [{
-  value: "3-sessions",
-  condition: "Trois séances validées avec DeepMobility.",
-  congrats: "Trois séances validées! Vous avez compris que quelques minutes par jour font la différence. Gardez le rythme !",
+  value: "3-in-1-day",
+  condition: "Trois séances en un jour validées.",
+  congrats: "Trois séances en un jour validées ! Vous avez compris que quelques minutes par jour font la différence. Gardez le rythme !",
 }, {
   value: "1-full-week",
   condition: "Premier parcours validé.",
-  congrats: "Félicitations, ce badge marque une semaine complète de sessions, preuve que vous intégrez déjà le mouvement dans votre quotidien. Continuez sur cette lancée !",
+  congrats: "Félicitations, ce badge marque une semaine complète de routine, preuve que vous intégrez déjà le mouvement dans votre quotidien. Continuez sur cette lancée !",
 }, {
   value: "90min",
-  condition: "Cumulé 1h30 de pratique avec DeepMobility.",
-  congrats: "Bravo pour ces 1h30 de mobilité avec DeepMobility. Ce badge récompense votre engagement à bouger régulièrement. Continuez sur cette lancée.",
+  condition: "Cumulé 1h30 de routine musculaire.",
+  congrats: "Bravo pour ces 1h30 de mobilité avec DeepMobility. Ce badge récompense votre engagement à bouger régulièrement. Continuez sur cette lancée !",
 }, {
   value: "5-full-week",
-  condition: "5 parcours validés",
-  congrats: "Vous faites partie des utilisateurs réguliers de DeepMobility ! Ce badge récompense ceux qui ont validé 5 semaines de DeepMobility. Vous êtes un modèle d’engagement !",
+  condition: "5 parcours consécutifs.",
+  congrats: "Vous faites partie des utilisateurs réguliers de DeepMobility ! Ce badge récompense ceux qui ont validé 5 semaines consécutives. Vous êtes un modèle d’engagement !",
 }]
 
 export default badgesList
