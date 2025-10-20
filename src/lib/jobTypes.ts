62f4b693a66e36753a10233105d1ecbf80fc0d3a
forster <groux.forster@gmail.com>
2025-10-20 09:21:59 +0200

    update job types

--- src/lib/jobTypes.ts
+++ src/lib/jobTypes.ts
@@ -1,8 +1,8 @@
 const jobTypes = [
   { value: 'standing', label: 'Travail debout prolongé' },
   { value: 'repetitive-gestures', label: 'Travail physique avec port de charge' },
-  { value: 'sitting', label: 'Travail sédentaire (assis la majorité du temps)' },
-  { value: 'remote', label: 'Télétravail ou flex office' },
+  { value: 'sitting', label: 'Travail assis et/ou debout avec gestes répétitifs' },
+  { value: 'remote', label: 'Travail sédentaire (assis la majorité du temps)' },
 ]
 
 export default jobTypes
